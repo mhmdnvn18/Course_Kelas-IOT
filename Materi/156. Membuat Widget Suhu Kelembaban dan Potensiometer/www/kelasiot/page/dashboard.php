caf7c07af2fa899b986bfc4c742c494bdf62b514
anop <81241987+mhmdnvn18@users.noreply.github.com>
2025-01-27 15:11:23 +0700

    update

---
+++ Materi/156. Membuat Widget Suhu Kelembaban dan Potensiometer/www/kelasiot/page/dashboard.php
@@ -0,0 +1,73 @@
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Dashboard</h1>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-lg-4">
+          <!-- small card -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>23 C</h3>
+
+                <p>Suhu</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-temperature-high"></i>
+              </div>
+              <a href="#" class="small-box-footer">
+                More info <i class="fas fa-arrow-circle-right"></i>
+              </a>
+            </div>
+          </div>
+          <div class="col-lg-4">
+          <!-- small card -->
+            <div class="small-box bg-indigo">
+              <div class="inner">
+                <h3>75 %</h3>
+
+                <p>Kelembaban</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-water"></i>
+              </div>
+              <a href="#" class="small-box-footer">
+                More info <i class="fas fa-arrow-circle-right"></i>
+              </a>
+            </div>
+          </div>
+          <div class="col-lg-4">
+          <!-- small card -->
+            <div class="small-box bg-maroon">
+              <div class="inner">
+                <h3>20</h3>
+
+                <p>Potentiometer</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-tachometer-alt"></i>
+              </div>
+              <a href="#" class="small-box-footer">
+                More info <i class="fas fa-arrow-circle-right"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+  </div>
+<!-- /.content-wrapper -->
---
+++ Materi/157. Membuat Slider/www/kelasiot/page/dashboard.php
@@ -0,0 +1,92 @@
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Dashboard</h1>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-lg-4">
+          <!-- small card -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>23 C</h3>
+
+                <p>Suhu</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-temperature-high"></i>
+              </div>
+              <a href="#" class="small-box-footer">
+                More info <i class="fas fa-arrow-circle-right"></i>
+              </a>
+            </div>
+          </div>
+          <div class="col-lg-4">
+          <!-- small card -->
+            <div class="small-box bg-indigo">
+              <div class="inner">
+                <h3>75 %</h3>
+
+                <p>Kelembaban</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-water"></i>
+              </div>
+              <a href="#" class="small-box-footer">
+                More info <i class="fas fa-arrow-circle-right"></i>
+              </a>
+            </div>
+          </div>
+          <div class="col-lg-4">
+          <!-- small card -->
+            <div class="small-box bg-maroon">
+              <div class="inner">
+                <h3>20</h3>
+
+                <p>Potentiometer</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-tachometer-alt"></i>
+              </div>
+              <a href="#" class="small-box-footer">
+                More info <i class="fas fa-arrow-circle-right"></i>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-6">
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Servor</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <div class="row margin">
+                  <div class="col-sm-12">
+                    <input id="servo" type="text" name="range_6" value="">
+                  </div>
+                </div>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+  </div>
+<!-- /.content-wrapper -->
